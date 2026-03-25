98f967b414e30932307c8368de024f8c0be0de64
Lloyd-Pottiger <yan1579196623@gmail.com>
2026-03-25 13:48:38 +0800

    tikv: re-export global helpers

--- src/tikv.rs
+++ src/tikv.rs
@@ -5,3 +5,20 @@
 
 pub use crate::Backoff as Backoffer;
 pub use crate::TransactionClient as KVStore;
+
+#[doc(inline)]
+pub use crate::disable_resource_control;
+#[doc(inline)]
+pub use crate::enable_resource_control;
+#[doc(inline)]
+pub use crate::set_resource_control_interceptor;
+#[doc(inline)]
+pub use crate::unset_resource_control_interceptor;
+
+#[doc(inline)]
+#[allow(deprecated)]
+pub use crate::set_region_cache_ttl;
+#[doc(inline)]
+pub use crate::set_region_cache_ttl_with_jitter;
+#[doc(inline)]
+pub use crate::set_store_liveness_timeout;
--- tests/tikv_module_public_api.rs
+++ tests/tikv_module_public_api.rs
@@ -1,7 +1,24 @@
+use std::sync::Arc;
+use std::time::Duration;
+
 use tikv_client::tikv;
 
 #[test]
 fn tikv_module_exports_kvstore_and_backoffer() {
     let _: Option<tikv::KVStore> = None;
     let _ = tikv::Backoffer::no_backoff();
+}
+
+#[test]
+fn tikv_module_exports_global_helpers() {
+    let _: fn(Duration, Duration) = tikv::set_region_cache_ttl_with_jitter;
+    let _: fn(Duration) = tikv::set_store_liveness_timeout;
+    #[allow(deprecated)]
+    let _: fn(Duration) = tikv::set_region_cache_ttl;
+
+    let _: fn() = tikv::enable_resource_control;
+    let _: fn() = tikv::disable_resource_control;
+    let _: fn(Arc<dyn tikv_client::ResourceGroupKvInterceptor>) =
+        tikv::set_resource_control_interceptor;
+    let _: fn() = tikv::unset_resource_control_interceptor;
 }
