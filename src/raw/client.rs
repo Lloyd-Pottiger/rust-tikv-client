38933be3bbfc7035cd66d1bc465304adead96494
Lloyd-Pottiger <yan1579196623@gmail.com>
2026-03-28 05:14:19 +0800

    test raw atomic put ttl dispatch

--- src/raw/client.rs
+++ src/raw/client.rs
@@ -2188,6 +2188,44 @@ mod tests {
     }
 
     #[tokio::test]
+    async fn test_atomic_put_with_ttl_propagates_for_cas_and_explicit_ttl() -> Result<()> {
+        let dispatch_calls = Arc::new(AtomicUsize::new(0));
+        let dispatch_calls_captured = dispatch_calls.clone();
+
+        let pd_client = Arc::new(MockPdClient::new(MockKvClient::with_dispatch_hook(
+            move |req: &dyn Any| {
+                let req = req
+                    .downcast_ref::<kvrpcpb::RawPutRequest>()
+                    .expect("expected raw put request");
+                assert_eq!(req.key, vec![42]);
+                assert_eq!(req.value, vec![100]);
+                assert_eq!(req.ttl, 9);
+                assert_eq!(req.cf, "lock");
+                assert!(req.for_cas);
+                dispatch_calls_captured.fetch_add(1, Ordering::SeqCst);
+                Ok(Box::new(kvrpcpb::RawPutResponse::default()) as Box<dyn Any>)
+            },
+        )));
+
+        let client = Client {
+            safe_ts: SafeTsCache::new(pd_client.clone(), Keyspace::Disable),
+            rpc: pd_client,
+            cf: Some(ColumnFamily::Lock),
+            backoff: DEFAULT_REGION_BACKOFF,
+            atomic: false,
+            trace_id: None,
+            trace_control_flags: TraceControlFlags::default(),
+            keyspace: Keyspace::Disable,
+        }
+        .with_atomic_for_cas();
+
+        client.put_with_ttl(vec![42], vec![100], 9).await?;
+
+        assert_eq!(dispatch_calls.load(Ordering::SeqCst), 1);
+        Ok(())
+    }
+
+    #[tokio::test]
     async fn test_compare_and_swap_requires_atomic_mode() {
         let dispatch_calls = Arc::new(AtomicUsize::new(0));
         let dispatch_calls_captured = dispatch_calls.clone();
