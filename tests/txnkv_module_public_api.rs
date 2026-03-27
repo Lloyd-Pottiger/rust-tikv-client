abf2414561f94acfd50973e708bac27ebb3f8f3c
Lloyd-Pottiger <yan1579196623@gmail.com>
2026-03-27 21:43:51 +0800

    test txnkv nested exports

--- tests/txnkv_module_public_api.rs
+++ tests/txnkv_module_public_api.rs
@@ -9,8 +9,28 @@ fn txnkv_module_exports_types() {
     let _: Option<txnkv::TxnStatus> = None;
 
     let _: Option<txnkv::transaction::Transaction> = None;
+    let _: Option<txnkv::transaction::TransactionOptions> = None;
+    let _: Option<txnkv::transaction::AssertionLevel> = None;
+    let _: Option<txnkv::transaction::LockWaitTimeout> = None;
+    let _: Option<txnkv::transaction::PipelinedTxnOptions> = None;
+    let _: Option<txnkv::transaction::PrewriteEncounterLockPolicy> = None;
+
     let _: Option<txnkv::txnsnapshot::Snapshot> = None;
+    let _: Option<txnkv::txnsnapshot::SyncSnapshot> = None;
+
     let _: Option<txnkv::txnlock::LockResolver> = None;
+    let _: Option<txnkv::txnlock::Lock> = None;
+    let _: Option<txnkv::txnlock::ResolveLockDetail> = None;
+    let _: Option<txnkv::txnlock::ResolveLocksContext> = None;
+    let _: Option<txnkv::txnlock::ResolveLocksForReadResult> = None;
+    let _: Option<txnkv::txnlock::ResolveLocksOptions> = None;
+    let _: Option<txnkv::txnlock::ResolveLocksResult> = None;
+    let _: Option<txnkv::txnlock::ResolvingLock> = None;
+
     let _: Option<txnkv::rangetask::DeleteRangeTask> = None;
+    let _: Option<txnkv::rangetask::RangeTaskRunner<tikv_client::PdRpcClient>> = None;
+    let _: Option<txnkv::rangetask::RangeTaskStat> = None;
+    let _: Option<std::sync::Arc<dyn txnkv::rangetask::RangeTaskHandler>> = None;
+
     let _: Option<txnkv::txnutil::TxnStatus> = None;
 }
