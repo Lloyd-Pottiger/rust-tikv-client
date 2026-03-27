276dbf1290aa4cd9ec013f6ee9cd1098c049e421
Lloyd-Pottiger <yan1579196623@gmail.com>
2026-03-27 21:11:14 +0800

    test kv module helper api

--- tests/kv_module_public_api.rs
+++ tests/kv_module_public_api.rs
@@ -34,6 +34,11 @@ fn kv_module_exports_store_vars_and_replica_read_type() {
 
 #[test]
 fn kv_module_exports_get_and_batch_get_option_types() {
+    let shared = kv::GetOrBatchGetOption::ReturnCommitTs;
+    let _: kv::GetOption = shared;
+    let _: kv::BatchGetOption = shared;
+    let _: kv::GetOrBatchGetOption = kv::with_return_commit_ts();
+
     let mut opts = kv::GetOptions::default();
     opts.apply(&[kv::with_return_commit_ts()]);
     assert!(opts.return_commit_ts());
@@ -71,6 +76,19 @@ fn kv_module_exports_key_helpers_and_key_range() {
 
     let _ = kv::Key::from(b"k".to_vec()).next_key();
     let _ = kv::Key::from(b"k".to_vec()).prefix_next_key();
+}
+
+#[test]
+fn kv_module_exports_hex_repr_and_kv_pair_ttl() {
+    assert_eq!(format!("{}", kv::HexRepr(&[0x00, 0xAB, 0x10])), "00AB10");
+
+    let pair = kv::KvPair::new(b"k".to_vec(), b"v".to_vec());
+    let ttl_pair = kv::KvPairTTL(pair.clone().into(), 42);
+    assert_eq!(ttl_pair.as_ref(), pair.key());
+
+    let (roundtrip_pair, ttl): (tikv_client::proto::kvrpcpb::KvPair, u64) = ttl_pair.into();
+    assert_eq!(kv::KvPair::from(roundtrip_pair), pair);
+    assert_eq!(ttl, 42);
 }
 
 #[test]
