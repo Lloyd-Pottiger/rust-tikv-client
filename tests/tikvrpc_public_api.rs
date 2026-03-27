6c1d63bb3cce79503a2c165bb07110aaa6d62fa5
Lloyd-Pottiger <yan1579196623@gmail.com>
2026-03-27 16:57:14 +0800

    Expand tikvrpc public API coverage

--- tests/tikvrpc_public_api.rs
+++ tests/tikvrpc_public_api.rs
@@ -2,6 +2,7 @@ use tikv_client::tikvrpc;
 
 #[test]
 fn tikvrpc_module_exports_request_and_response_wrappers() {
+    let _: tikvrpc::CmdType = tikvrpc::CmdType::Get;
     let _: Option<tikvrpc::Request> = None;
 
     #[derive(Debug)]
@@ -12,7 +13,17 @@ fn tikvrpc_module_exports_request_and_response_wrappers() {
     let response = MockResponse { value: 42 };
     let wrapper = tikvrpc::Response::new("kv_get", &response);
     assert_eq!(wrapper.label(), "kv_get");
-    let _ = wrapper.cmd_type();
+    assert_eq!(wrapper.cmd_type(), tikvrpc::CmdType::Get);
     assert_eq!(wrapper.downcast_ref::<MockResponse>().unwrap().value, 42);
     assert!(wrapper.downcast_ref::<u32>().is_none());
+
+    assert_eq!(
+        tikvrpc::CmdType::from_label("kv_get"),
+        tikvrpc::CmdType::Get
+    );
+    assert_eq!(
+        tikvrpc::CmdType::from_label("not-a-real-label"),
+        tikvrpc::CmdType::Unknown
+    );
+    assert_eq!(tikvrpc::CmdType::Commit.as_str(), "Commit");
 }
